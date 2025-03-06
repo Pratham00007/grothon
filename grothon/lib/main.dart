71054080759719f83ff41eebb571fa826490d998
Pratham <pratham10600@gmail.com>
2025-03-06 20:11:52 +0530

    firebase added

--- grothon/lib/main.dart
+++ grothon/lib/main.dart
@@ -1,12 +1,18 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'package:grothon/providers/shop_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:grothon/screens/login%20and%20signup/login.dart';
 
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(
       providers: [
